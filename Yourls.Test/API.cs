3296ccef7713d789905f8143b3b9426182bcf7de
Rob Smith <smith.rob.s@gmail.com>
2012-04-21 08:09:36 -0700

    initial commit

---
+++ Yourls.Test/API.cs
@@ -0,0 +1,88 @@
+﻿using Xunit;
+
+namespace Yourls.Test
+{
+    public class API
+    {
+        public readonly string yourls_url = "ph.ly";
+        public readonly string yourls_api = "1a40d1e654";
+        public readonly YourlsClient client;
+
+        public API()
+        {
+            client = new YourlsClient(yourls_url, yourls_api);
+        }
+
+        [Fact]
+        public void should_shorten_url()
+        {
+            var result = client.Shorten("http://www.amazon.com/Dusk-Subside-Inverloch/dp/B007O4UE42");
+
+            //{"url":{"keyword":"1axuf","url":"http:\/\/www.amazon.com\/Dusk-Subside-Inverloch\/dp\/B007O4UE42","title":"http:\/\/www.amazon.com\/Dusk-Subside-Inverloch\/dp\/B007O4UE42","date":"2012-04-21 08:03:30","ip":"96.245.169.72"},"status":"success","message":"http:\/\/www.amazon.com\/Dusk-Subside-Inverloch\/dp\/B007O4UE42 added to database","title":"http:\/\/www.amazon.com\/Dusk-Subside-Inverloch\/dp\/B007O4UE42","shorturl":"http:\/\/ph.ly\/1axuf","statusCode":200}
+
+            Assert.Equal(200, (int)result.statusCode);
+        }
+
+        [Fact]
+        public void should_not_shorten_existing_url()
+        {
+            var result = client.Shorten("https://github.com/gabrielpreston/node-yourls");
+
+            Assert.Equal("fail", (string)result.status);
+            Assert.Equal(@"error:url", (string)result.code);
+            Assert.Equal(200, (int)result.statusCode);
+        }
+
+        [Fact]
+        public void should_expand_valid_full_url()
+        {
+            var result = client.Expand("http://ph.ly/dzg-v");
+
+            Assert.Equal(200, (int)result.statusCode);
+            Assert.Equal("dzg-v", (string)result.keyword);
+            Assert.Equal(@"http://ph.ly/dzg-v", (string)result.shorturl);
+            Assert.Equal(@"https://github.com/gabrielpreston/node-yourls", (string)result.longurl);
+        }
+
+        [Fact]
+        public void should_expand_valid_hash()
+        {
+            var result = client.Expand("dzg-v");
+
+            Assert.Equal(200, (int)result.statusCode);
+            Assert.Equal("dzg-v", (string)result.keyword);
+            Assert.Equal(@"http://ph.ly/dzg-v", (string)result.shorturl);
+            Assert.Equal(@"https://github.com/gabrielpreston/node-yourls", (string)result.longurl);
+        }
+
+        [Fact]
+        public void should_get_urlstats_valid_url()
+        {
+            var result = client.UrlStats("http://ph.ly/dzg-v");
+
+            Assert.Equal(200, (int)result.statusCode);
+            Assert.Equal(@"http://ph.ly/dzg-v", (string)result.link.shorturl);
+            Assert.Equal(@"https://github.com/gabrielpreston/node-yourls", (string)result.link.url);
+            Assert.Equal(@"gabrielpreston/node-yourls · GitHub", (string)result.link.title);
+            Assert.Equal("2012-04-18 15:00:56", (string)result.link.timestamp);
+            Assert.Equal("204.236.242.146", (string)result.link.ip);
+            Assert.True((int)result.link.clicks > 30);
+        }
+
+        [Fact]
+        public void should_get_urlstats_valid_hash()
+        {
+            var result = client.UrlStats("dzg-v");
+
+            Assert.Equal(200, (int)result.statusCode);
+            Assert.Equal(@"http://ph.ly/dzg-v", (string)result.link.shorturl);
+            Assert.Equal(@"https://github.com/gabrielpreston/node-yourls", (string)result.link.url);
+            Assert.Equal(@"gabrielpreston/node-yourls · GitHub", (string)result.link.title);
+            Assert.Equal("2012-04-18 15:00:56", (string)result.link.timestamp);
+            Assert.Equal("204.236.242.146", (string)result.link.ip);
+            Assert.True((int)result.link.clicks > 30);
+        }
+
+        // do tests on invalid full url/hash?
+    }
+}
---
+++ Yourls/YourlsClient.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Json;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Yourls
+{
+    /// <summary>
+    /// yourls - A node module for calling the yourls API
+    /// See http://yourls.org/#API for details
+    /// about the API requests and responses
+    /// </summary>
+    public class YourlsClient
+    {
+        private class Config
+        {
+            public string api_token { get; set; }
+            public string api_url { get; set; }
+            public string format { get; set; }
+        }
+
+        private Config _config;
+
+        /// <summary>
+        /// The main yourls constructor, takes the yourls api url, api token and additional options
+        /// </summary>
+        /// <param name="url">The URL of the Yourls service being used</param>
+        /// <param name="apiKey">The users API token</param>
+        public YourlsClient(string url, string apiKey)
+        {
+            _config = new Config
+                            {
+                                api_url = url,
+                                api_token = apiKey,
+                                format = "json"
+                            };
+        }
+
+        /// <summary>
+        /// Generate querystring from a dictionary
+        /// </summary>
+        /// <param name="items">Dictionary of string key values</param>
+        /// <returns></returns>
+        private string ToQueryString(Dictionary<string, string> items)
+        {
+            return "?" + string.Join("&", items.Select(kv => string.Format("{0}={1}", HttpUtility.UrlEncode(kv.Key), HttpUtility.UrlEncode(kv.Value))).ToArray());
+        }
+
+        /// <summary>
+        /// Generates the URL object to be passed to the HTTP request for a specific
+        /// API method call
+        /// </summary>
+        /// <param name="query">The query object</param>
+        /// <returns>The URL object for this request</returns>
+        private Uri GenerateNiceUrl(Dictionary<string, string> query)
+        {
+            return new UriBuilder("http", _config.api_url, 80, "/yourls-api.php", ToQueryString(query)).Uri;
+        }
+
+        /// <summary>
+        /// Function to do a HTTP Get request with the current query
+        /// </summary>
+        /// <param name="request">The current request uri</param>
+        /// <returns>JSON response</returns>
+        private JsonValue DoRequest(Uri request)
+        {
+            var req = WebRequest.Create(request);
+            string responseFromServer;
+            using (var response = (HttpWebResponse)req.GetResponse())
+            using (var dataStream = response.GetResponseStream())
+            using (var reader = new StreamReader(dataStream))
+            {
+                responseFromServer = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
+            return JsonValue.Parse(responseFromServer);
+        }
+
+        /// <summary>
+        /// Request to shorten one long url
+        /// </summary>
+        /// <param name="longUrl">The URL to be shortened</param>
+        /// <returns>Result</returns>
+        public dynamic Shorten(string longUrl)
+        {
+            var query = new Dictionary<string, string>
+                            {
+                                {"signature", _config.api_token},
+                                {"format", _config.format},
+                                {"url", longUrl},
+                                {"action", "shorturl"}
+                            };
+
+            return DoRequest(GenerateNiceUrl(query)).AsDynamic();
+        }
+
+        /// <summary>
+        /// Request to shorten one long url and return a vanity url
+        /// </summary>
+        /// <param name="longUrl">The URL to be shortened</param>
+        /// <param name="vanityName">The requested vanity url</param>
+        /// <returns>Result</returns>
+        public dynamic Vanity(string longUrl, string vanityName)
+        {
+            var query = new Dictionary<string, string>
+                            {
+                                {"signature", _config.api_token},
+                                {"format", _config.format},
+                                {"url", longUrl},
+                                {"keyword", vanityName},
+                                {"action", "shorturl"}
+                            };
+
+            return DoRequest(GenerateNiceUrl(query)).AsDynamic();
+        }
+
+        /// <summary>
+        /// Request to expand a single short url or hash
+        /// </summary>
+        /// <param name="item">The short url or hash to expand</param>
+        /// <returns>Result</returns>
+        public dynamic Expand(string item)
+        {
+            var query = new Dictionary<string, string>
+                            {
+                                {"signature", _config.api_token},
+                                {"format", _config.format},
+                                {"shorturl", item},
+                                {"action", "expand"}
+                            };
+
+            return DoRequest(GenerateNiceUrl(query)).AsDynamic();
+        }
+
+        /// <summary>
+        /// Request to retrieve stats on a specific short url/hash
+        /// </summary>
+        /// <param name="item">The short url or hash to get stats on</param>
+        /// <returns>Result</returns>
+        public dynamic UrlStats(string item)
+        {
+            var query = new Dictionary<string, string>
+                            {
+                                {"signature", _config.api_token},
+                                {"format", _config.format},
+                                {"shorturl", item},
+                                {"action", "url-stats"}
+                            };
+
+            return DoRequest(GenerateNiceUrl(query)).AsDynamic();
+        }
+    }
+}
